358d6417815d8dddae64b7219fda06a743d65bfb
axcel0773 <yuji-watanabe1@hotmail.com>
2021-10-16 16:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ VolumeEsfera/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VolumeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            double volume;
+
+            Console.WriteLine("Digite o raio da esfera");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            volume =  (4 * (3.14) * Math.Pow(raio, 3)) / 3;
+            Console.WriteLine("O valor do volume da esfera é {0}", volume);
+        }
+    }
+}
